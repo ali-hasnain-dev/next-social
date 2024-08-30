12efaa1f2121fde1b37680b2a5cad2d620a7beee
unknown <alihasnainhfd@gmail.com>
2024-08-31 00:15:07 +0500

    initial commit

---
+++ src/components/MobileMenue.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const MobileMenue = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="md:hidden">
+      <div
+        className="flex flex-col gap-[4.5px] cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          className={`w-6  h-1 bg-blue-600 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-600 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          } ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-600 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+      </div>
+      {isOpen && (
+        <div className="absolute left-0 top-24 w-full h-[calc(100vh-98px)] bg-white flex items-center flex-col justify-center gap-8 text-lg font-semibold z-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Friends</Link>
+          <Link href="/">Groups</Link>
+          <Link href="/">Stories</Link>
+          <Link href="/">Login</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MobileMenue;
