c966e2097fd5bef16c4fd008db11d85995ee658f
unknown <alihasnainhfd@gmail.com>
2024-09-02 18:12:04 +0500

    left side menue list

--- src/components/LeftMenue.tsx
+++ src/components/LeftMenue.tsx
@@ -1,7 +1,96 @@
 import React from "react";
+import ProfileCard from "./ProfileCard";
+import Link from "next/link";
+import Image from "next/image";
 
-const LeftMenue = () => {
-  return <div>LeftMenue</div>;
+const LeftMenue = ({ type }: { type: "home" | "profile" }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      {type === "home" && <ProfileCard />}
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/posts.png" width={20} height={20} alt="Posts" />
+          <span>My posts</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/activity.png" width={20} height={20} alt="Posts" />
+          <span>Activity</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/market.png" width={20} height={20} alt="Posts" />
+          <span>MarketPlace</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/events.png" width={20} height={20} alt="Posts" />
+          <span>Events</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/albums.png" width={20} height={20} alt="Posts" />
+          <span>Albums</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/videos.png" width={20} height={20} alt="Posts" />
+          <span>Videos</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/news.png" width={20} height={20} alt="Posts" />
+          <span>News</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/courses.png" width={20} height={20} alt="Posts" />
+          <span>Courses</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/lists.png" width={20} height={20} alt="Posts" />
+          <span>Lists</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image src="/settings.png" width={20} height={20} alt="Posts" />
+          <span>Settings</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center" />
+      </div>
+    </div>
+  );
 };
 
 export default LeftMenue;
